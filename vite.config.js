e15862b89ce36312e287a45f469790c37154d695
Rk-Dinesh <rdkumar881@gmail.com>
2024-07-09 20:03:58 +0530

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Adjust according to your project's needs
+          vendor: ['react', 'react-dom'],
+         
+          // Add other chunks as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+  },
 })
